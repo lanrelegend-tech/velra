687233e5d7792f50897285f0c8abc66debe7bb61
Olanrewaju <lanrelegend@gmail.com>
2026-04-20 10:02:50 -0700

    build error fix

--- src/app/components/ConditionalNavbar.jsx
+++ src/app/components/ConditionalNavbar.jsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import Navbar from "./Navbar";
+import dynamic from "next/dynamic";
+
+const Navbar = dynamic(() => import("./Navbar"), {
+  ssr: false,
+});
 
 export default function ConditionalNavbar() {
   const pathname = usePathname();
